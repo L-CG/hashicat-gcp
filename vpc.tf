d3ad8ff6c05ee52ffbdd50a520e33d2ac1518ae2
L-CG <l.cuevaguerrero@reply.de>
2022-01-10 12:33:12 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,15 @@
 module "network" {
-  				#source  = "app.terraform.io/HashiCat_GCP/network/google"
-  				source = "https://app.terraform.io/app/HashiCat_GCP/registry/modules/private/HashiCat_GCP/network/google/3.4.0?tab=inputs"
-                version = "3.4.0"
-  				network_name = gaurav-network 
-  				project_id = var.project
+  				source  = "app.terraform.io/HashiCat_GCP/network/google"
+  				#source = "https://app.terraform.io/app/HashiCat_GCP/registry/modules/private/HashiCat_GCP/network/google/3.4.0?tab=inputs"
+          version = "3.4.0"
+  				network_name = "gaurav-network" 
+  				project_id = "var.project"
+
   				subnets = [
   				  {
     				subnet_name   = "gaurav-subnet"
     				subnet_ip     = "10.100.10.0/24"
-    				subnet_region = var.region
+    				subnet_region = "var.region"
   				  }
 				]
 			}
